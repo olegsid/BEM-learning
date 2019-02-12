313483c226a498501a32d4746a8734a574c3e0bc
Oleg Shevchuk <oleg.shevchuk@viseven.com>
2019-02-12 10:19:10 +0200

    update(webpack.config.js): html formating

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
           html: {
               end_with_newline: true,
               indent_size: 2,
-              indent_with_tabs: true,
+              indent_with_tabs: false,
               indent_inner_html: true,
               preserve_newlines: true,
               inline: ['i', 'b'],
